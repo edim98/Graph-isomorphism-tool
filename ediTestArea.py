8b93019e2d730881bd4b5e4e7f9faf32cb7f8a42
Eduard Constantinescu <e.constantinescu@student.utwente.nl>
2019-04-03 16:16:49 +0200

    jk andrei te pwp

--- ediTestArea.py
+++ ediTestArea.py
@@ -1,17 +1,39 @@
 from graph_io import load_graph, save_graph, write_dot
 from graph import *
-from refined_colouring import refine_colour, nicePrinting
+from refined_colouring import refine_colour, nicePrinting, colourGraph
+from individualization import testDisjointUnion
 import time
 
-startTime = time.time()
-
-with open('threepaths5.gr') as f:
+with open("test.gr") as f:
     G = load_graph(f)
 
+# with open("test.gr") as f:
+#     G = load_graph(f)
+#
+# with open("test.gr") as g:
+#     H = load_graph(g)
+#
+startTime = time.time()
+#
 res = refine_colour(G, [])
+
+#
+# GuG = testDisjointUnion(G, H)
+initial_colouring = [0]*(len(G))
+initial_colouring[0] = 1
+# # initial_colouring[10] = 1
+initial_colouring[2] = 2
+# # initial_colouring[13] = 2
+# res = refine_colour(GuG, [])
 
 endTime = time.time()
 
+# newG = colourGraph(res, GuG)
+
+with open("output.dot", "w") as x:
+    # write_dot(newG, x)
+    write_dot(G, x)
+    # write_dot(GuG, x)
 print('Time taken : ', endTime - startTime)
 nicePrinting(res)
 # print(G)
